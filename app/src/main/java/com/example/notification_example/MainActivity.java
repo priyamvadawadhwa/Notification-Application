cebcdef67bab76dfede9ed69734da6a3f548aa86
priyamvada <priyam.wadhwa04@gmail.com>
2021-05-20 22:04:33 +0530

    Notification Example

---
+++ app/src/main/java/com/example/notification_example/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.notification_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.example.notification_example.App.CHANNEL_1_ID;
+
+public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager ;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
+    }
+    public void sendOnChannel1(View v) {
+        String title = editTextMessage.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_one)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManager.notify(1,notification);
+    }
+}
